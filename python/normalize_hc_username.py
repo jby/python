3316162ec30e79269501f99d2d99320c14c44146
Jonas Bygdén <github@bygden.nu>
2019-05-21 13:23:15 +0200

    Cleaned up a bit

--- python/normalize_hc_username.py
+++ python/normalize_hc_username.py
@@ -6,16 +6,18 @@ username_by_email={}
 with open ("ad-users.csv", "r") as AD_users:
     ad_users = csv.reader(AD_users, delimiter=',')
     for ad_user in ad_users:
-        username_by_email[ad_user[2]] = ad_user[1]
+        username_by_email[ad_user[3]] = ad_user[0]
 
 with open ("users.json", "r") as HC_users:
     hc_users = json.load(HC_users)
     for hc_user in hc_users:
-        if hc_user["User"]["is_deleted"] == False:
-            try:
-                hc_user["User"]["mention_name"]=username_by_email[hc_user["User"]["email"]]
-            except KeyError:
-                pass
+        user=hc_user['User']
+        if ('is_deleted' in user and user["is_deleted"] == True):
+            continue
+        if user['email'] not in username_by_email:
+            continue
+        user["mention_name"]=username_by_email[user["email"]]
+        print('User: %24s"\t"Mention Name: %s' % (user['name'],user['mention_name']))
 
 with open ("users_modified.json", "w") as f:
     f.write(json.dumps(hc_users, indent=4))
